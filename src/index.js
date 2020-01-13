0e003c6257e5def79c1bc9f7c68b1c6e03b69a66
Mozuffer Hago <mozufferhago@gmail.com>
2020-01-13 17:16:51 +0400

    update the src code

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ var os = require("os");
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>Hello from ... <font color="red">' + os.hostname()+'</font></h1>');
+  res.end('<h1>Hello from ... v1 <font color="red">' + os.hostname()+'</font></h1>');
 }).listen(3000);
