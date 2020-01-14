4818ea0280890709acef37477e291c25deaa471e
Mozuffer Hago <mozufferhago@gmail.com>
2020-01-14 10:54:16 +0400

    update nodejs app

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ var os = require("os");
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>Hello from ... v5  <font color="red">' + os.hostname()+'</font></h1>');
+  res.end('<h1>Hello from ... v6  <font color="red">' + os.hostname()+'</font></h1>');
 }).listen(3000);
